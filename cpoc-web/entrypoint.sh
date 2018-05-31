1452d35f4e8ca1b814de214770a1827dc173102f
thanatchakromsang <thanatchakromsang@gmail.com>
2018-05-31 14:55:59 +0700

    Initial commit

---
+++ cpoc-web/entrypoint.sh
@@ -0,0 +1,6 @@
+#/usr/bin/env bash
+
+cd $APPLICATION_ROOT\cpoc_web
+while ! nc -z db 5432; do sleep 1; done
+python manage.py migrate --settings=cpoc_web.settings
+python manage.py runserver 0.0.0.0:8000 --cpoc_web.settings
