e29e41ef313eba7185c7b998ea042f5e3a5f3857
parnpresso <parnpresso@gmail.com>
2018-05-31 15:08:46 +0700

    Fix parameter :crown:

--- cpoc-web/entrypoint.sh
+++ cpoc-web/entrypoint.sh
@@ -3,4 +3,4 @@
 cd $APPLICATION_ROOT\cpoc_web
 while ! nc -z db 5432; do sleep 1; done
 python manage.py migrate --settings=cpoc_web.settings
-python manage.py runserver 0.0.0.0:8000 --cpoc_web.settings
+python manage.py runserver 0.0.0.0:8000 --settings=cpoc_web.settings
