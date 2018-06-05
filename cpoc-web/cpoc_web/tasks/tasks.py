cce18d76275b72d230374d2fc7af050e594ed575
thanatchakromsang <thanatchakromsang@gmail.com>
2018-06-05 14:51:03 +0700

    Add tasks to test apply_async() :muscle:

--- cpoc-web/cpoc_web/tasks/tasks.py
+++ cpoc-web/cpoc_web/tasks/tasks.py
@@ -9,3 +9,18 @@ from celery import shared_task
 def create_random_word():
     random_word = get_random_string(12)
     return '{}'.format(random_word)
+
+
+@shared_task
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
