ee6914f410135427105fb1134a1746ba0e1dc606
parnpresso <parnpresso@gmail.com>
2018-06-06 14:00:21 +0700

    Add init file :crown:

--- cpoc-web/cpoc_web/tasks/views/task_a_view.py
+++ cpoc-web/cpoc_web/tasks/views/task_a_view.py
@@ -7,6 +7,7 @@ from tasks.tasks import create_random_word
 
 class TaskAView(APIView):
     def get(self, request):
+        create_random_word.apply_async()
         return Response({
             'status': 1
         })
